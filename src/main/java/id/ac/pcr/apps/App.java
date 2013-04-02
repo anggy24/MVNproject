a9316c233082ec467d3f27b16df5b5e194bb4662
Anggy <anggy@pcr.ac.id>
2013-04-02 08:22:49 +0700

    commit pertama

---
+++ src/main/java/id/ac/pcr/apps/App.java
@@ -0,0 +1,10 @@
+package id.ac.pcr.apps;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String a;
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/id/ac/pcr/apps/AppTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.pcr.apps;
+
+/**
+ *
+ * @author LAB316
+ */
+public class AppTest {
+    int a = 5;
+    int b = 7;
+    int c = a+b;
+    
+}
